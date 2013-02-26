f72d6ffbc14d6872863e79a6d7acba19653994d3
Bram Moolenaar <bram@vim.org>
2013-02-26 14:56:42 +0100

    updated for version 7.3.836 Problem:    Clipboard does not work on Win32 when compiled with Cygwin. Solution:   Move the Win32 clipboard code to a separate file and use it when 	    building with os_unix.c. (Frodak Baksik, Ken Takata)

--- src/feature.h
+++ src/feature.h
@@ -1137,6 +1137,11 @@
  * +xterm_clipboard	Unix only: Include code for handling the clipboard
  *			in an xterm like in the GUI.
  */
+
+#ifdef FEAT_CYGWIN_WIN32_CLIPBOARD
+# define FEAT_CLIPBOARD
+#endif
+
 #ifdef FEAT_GUI
 # ifndef FEAT_CLIPBOARD
 #  define FEAT_CLIPBOARD
