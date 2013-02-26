7eab42581e4dd2f37b6ccf33c12f59be2d90105e
Bram Moolenaar <bram@vim.org>
2013-02-26 15:27:23 +0100

    updated for version 7.3.837 Problem:    Empty lines in :version output when 'columns' is 320. Solution:   Simplify the logic of making columns. (Nazri Ramliy, Roland 	    Eggner)

--- src/version.c
+++ src/version.c
@@ -744,6 +744,8 @@ static char *(features[]) =
 static int included_patches[] =
 {   /* Add new patch number below this line */
 /**/
+    837,
+/**/
     836,
 /**/
     835,
@@ -2511,14 +2513,12 @@ list_features()
 	return;
     }
 
-    ncol = (int) Columns / width;
     /* The rightmost column doesn't need a separator.
      * Sacrifice it to fit in one more column if possible. */
-    if (Columns % width == width - 1)
-	ncol++;
-
+    ncol = (int) (Columns + 1) / width;
     nrow = nfeat / ncol + (nfeat % ncol ? 1 : 0);
 
+    /* i counts columns then rows.  idx counts rows then columns. */
     for (i = 0; !got_int && i < nrow * ncol; ++i)
     {
 	int idx = (i / ncol) + (i % ncol) * nrow;
@@ -2540,7 +2540,10 @@ list_features()
 	    }
 	}
 	else
-	    msg_putchar('\n');
+	{
+	    if (msg_col > 0)
+		msg_putchar('\n');
+	}
     }
 }
 
