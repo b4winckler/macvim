4e1c7f6402d44dfef9757d331bf54b68fc86993f
Christoffer Winterkvist <chris@zenangst.com>
2014-08-25 18:39:15 +0200

    Add Yosemite tab style

---
+++ src/MacVim/PSMTabBarControl/source/PSMYosemiteTabStyle.h
@@ -0,0 +1,14 @@
+//
+//  PSMYosemiteTabStyle.h
+//  PSMTabBarControl
+//
+//  Created by Christoffer Winterkvist on 25/08/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+#import "PSMTabStyle.h"
+
+@interface PSMYosemiteTabStyle : NSObject <PSMTabStyle>
+
+@end
---
+++ src/MacVim/PSMTabBarControl/source/PSMYosemiteTabStyle.m
@@ -0,0 +1,18 @@
+//
+//  PSMYosemiteTabStyle.m
+//  PSMTabBarControl
+//
+//  Created by Christoffer Winterkvist on 25/08/14.
+//
+//
+
+#import "PSMYosemiteTabStyle.h"
+
+@implementation PSMYosemiteTabStyle
+
+- (NSString *)name
+{
+    return @"Yosemite";
+}
+
+@end
